8ac6d1a8a46e3110b4dece61111d6d68d1839df4
Rodrigo Seborro <rodilool2@gmail.com>
2023-02-18 23:53:20 +0000

    added a funtion to randomly generate a number between the array length

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello world!')
+const part1 = ["You can ", "You will ", "You must "]
+const part2 = ["never stop,", "never give up,", "carry on fighting,","lift your head,"]
+const part3 = [" because you are strong!", " because you are unstoppable!", " because you are beautiful!", " because no one can hurt you!"]
+
+const randomNumber = arr =>{
+    return Math.floor(Math.random() * arr.length)
+};
